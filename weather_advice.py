e2b90ffb36ea10237b1ca1d0766a50bcf97ffb80
rachael <rolukowade@gmail.com>
2024-12-22 08:34:23 +0100

    add weather advice script

---
+++ weather_advice.py
@@ -0,0 +1,14 @@
+def main():
+    weather = input("what's the weather like today? (sunny/rainy/cold): ")
+
+    if weather == "sunny":
+      print ("wear a t-shirt and sunglasses")
+    elif weather == "rainy":
+       print ("don't forget your umbrella and a raincoat")
+    elif weather == "cold":
+       print ("make sure to wear a warm coat and a scarf")
+    else:
+       print ("sorry, i don't have recommendations for this weather.")
+
+if __name__ == "__main__":
+   main()
